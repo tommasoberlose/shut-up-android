61a155216fe9387bac5af884fcb2da60d6ab2c74
Tommaso Berlose <tommaso.berlose@gmail.com>
2018-01-02 17:12:03 +0100

    Fix Oreo support

--- app/src/main/java/com/tommasoberlose/shutup/service/GhostActivity.kt
+++ app/src/main/java/com/tommasoberlose/shutup/service/GhostActivity.kt
@@ -2,11 +2,14 @@ package com.tommasoberlose.shutup.service
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class GhostActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    Log.d("SHUT_UP", "3")
 
     ShutUpReceiver.shutUpThisPhone(this)
     finish()
--- app/src/main/java/com/tommasoberlose/shutup/service/ShutUpReceiver.kt
+++ app/src/main/java/com/tommasoberlose/shutup/service/ShutUpReceiver.kt
@@ -4,41 +4,51 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
+import android.util.Log
 import com.tommasoberlose.shutup.util.ConfigManager
 import com.tommasoberlose.shutup.util.ToastUtil
 
 class ShutUpReceiver : BroadcastReceiver() {
 
   override fun onReceive(context: Context, intent: Intent) {
+    Log.d("SHUT_UP", "OK")
     if (intent.action == ACTION_SHUT_UP) {
       val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_ALARM)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_ALARM)
       }
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_MUSIC)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_MUSIC)
       }
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_NOTIFICATION)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_NOTIFICATION)
       }
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_RING)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_RING, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_RING)
       }
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_SYSTEM)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_SYSTEM)
       }
       if (ConfigManager.isStreamSelected(context, AudioManager.STREAM_VOICE_CALL)) {
-        audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL, 0, 0)
+        silenceStrem(audioManager, AudioManager.STREAM_VOICE_CALL)
       }
       ToastUtil.showCustomView(context)
     }
+  }
+
+  fun silenceStrem(audioManager: AudioManager, stream: Int) {
+    try {
+      audioManager.setStreamVolume(stream, 0, 0)
+    } catch (ignored: Exception) { }
   }
 
   companion object {
     val ACTION_SHUT_UP = "com.tommasoberlose.shutup.action.SHUT_UP"
 
     fun shutUpThisPhone(context: Context) {
-      context.sendBroadcast(Intent(ACTION_SHUT_UP))
+      val i = Intent(ACTION_SHUT_UP)
+      i.setClass(context, ShutUpReceiver::class.java)
+      context.sendBroadcast(i)
     }
   }
 }
